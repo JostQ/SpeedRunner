cf36ef1417f9abd6604b99ec155c310ce43ed4b7
Maxime Simoncelli <36240090+maximesimoncelli@users.noreply.github.com>
2018-05-14 13:49:12 +0200

    Pages guard: on auth

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/////////////////////////////////////////
+///
+///           AUTH + HOMEPAGE
+///
+/////////////////////////////////////////
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +12,72 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')
+    ->name('home');
+
+/////////////////////////////////////////
+///
+///           PROFIL UTILISATEUR
+///
+/////////////////////////////////////////
+
+Route::get('/profile', 'ProfileController@index')
+    ->name('user_profile')
+    ->middleware('auth');
+Route::get('/profile/edit', 'ProfileController@edit')
+    ->name('user_profile_edit')
+    ->middleware('auth');
+
+Route::delete('/profile/delete', 'ProfileController@delete')
+    ->name('user_profile_delete')
+    ->middleware('auth');
+Route::put('/profile', 'ProfileController@update')
+    ->name('user_profile_update')
+    ->middleware('auth');
+
+/////////////////////////////////////////
+///
+///           PAGES
+///
+/////////////////////////////////////////
+
+// Page tutoriel GPX
+Route::get('/tutorial', 'TutorialController@index')
+    ->name('tutorial')
+    ->middleware('auth');
+// Fonction Amis
+Route::get('/friends', 'FriendsController@index')
+    ->name('friends')
+    ->middleware('auth');
+Route::post('/friends', 'FriendsController@add')
+    ->name('add_friend')
+    ->middleware('auth');
+Route::delete('/friends/{id}', 'FriendsController@delete')
+    ->name('delete_friend')
+    ->middleware('auth');
+//Statistiques
+Route::get('/statistics', 'StatisticsController@index')
+    ->name('statistics')
+    ->middleware('auth');
+//Fil actu
+Route::get('/actu', 'ActuController@index')
+    ->name('actu')
+    ->middleware('auth');
+//Classement
+Route::get('/leaderboards', 'LeaderboardsController@index')
+    ->name('leaderboards')
+    ->middleware('auth');
+//arbre progression
+Route::get('/progression', 'ProgressionController@index')
+    ->name('progression')
+    ->middleware('auth');
+
+/////////////////////////////////////////
+///
+///           ADMINISTRATION
+///
+/////////////////////////////////////////
+
+
+
+
