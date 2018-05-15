23924be38673341b766704c5770f2cb98ffd9c8f
Maxime Simoncelli <36240090+maximesimoncelli@users.noreply.github.com>
2018-05-15 16:17:16 +0200

    SUPER CSS !

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,28 +1,28 @@
 <footer class="py-5 bg-light">
     <div class="container">
         <div class="row">
-            <div class="col-lg-4 col-md-12">
-                <h3>Réseaux sociaux</h3>
+            <div class="col-lg-4 col-md-12 text-center">
+                <h4>Réseaux sociaux</h4>
                 <ul class="list-inline">
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-facebook-square"></i> Facebook</li></a>
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-twitter-square"></i> Twitter</li></a>
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-instagram"></i> Instragram</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-facebook-square"></i> Facebook</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-twitter-square"></i> Twitter</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-instagram"></i> Instragram</li></a>
                 </ul>
             </div>
-            <div class="col-lg-4 col-md-12">
-                <h3>Nous contacter</h3>
+            <div class="col-lg-4 col-md-12 text-center">
+                <h4>Nous contacter</h4>
                 <ul class="list-inline">
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-facebook-square"></i> Facebook</li></a>
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-twitter-square"></i> Twitter</li></a>
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-instagram"></i> Instragram</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-facebook-square"></i> Facebook</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-twitter-square"></i> Twitter</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-instagram"></i> Instragram</li></a>
                 </ul>
             </div>
-            <div class="col-lg-4 col-md-12">
-                <h3>Informations</h3>
+            <div class="col-lg-4 col-md-12 text-center">
+                <h4>Informations</h4>
                 <ul class="list-inline">
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-facebook-square"></i> Facebook</li></a>
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-twitter-square"></i> Twitter</li></a>
-                    <a href="/" class="h4 nav-link"><li><i class="fab fa-instagram"></i> Instragram</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-facebook-square"></i> Facebook</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-twitter-square"></i> Twitter</li></a>
+                    <a href="/" class="nav-link"><li><i class="fab fa-instagram"></i> Instragram</li></a>
                 </ul>
             </div>
         </div>
