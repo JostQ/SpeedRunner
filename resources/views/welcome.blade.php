c33ba284049a3332ae8c5524bcf983ada1f0e51b
Maxime Simoncelli <36240090+maximesimoncelli@users.noreply.github.com>
2018-05-15 12:14:14 +0200

    Landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,41 @@
 
 @section('content')
 
-    <div class="jumbotron-fluid" id="slider">
+    <div class="jumbotron-fluid pt-5 mt-5" id="slider">
+        <img class="position-fixed" style="z-index: -999" src="{{asset('images/1.jpg')}}" alt="">
         <div class="container py-4">
-            <h2 class="h2 text-light">Lorem ipsum dolor sit amet.</h2>
+            <h2 class="h2 text-light">Motivez-vous, bandes de merdes.</h2>
+            <div class="row py-4">
+                <div class="col-lg-4 col-md-6 col-12 text-center pb-4">
+                    <div class="card">
+                        <img class="p-5" src="{{asset('icons/jogging.svg')}}" alt="">
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda at aut blanditiis consectetur eos fuga in, maxime minima minus natus neque quis quo recusandae rem, rerum saepe sed tempora?
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-4 col-md-6 col-12 text-center pb-4">
+
+                    <div class="card">
+                        <img class="p-5" src="{{asset('icons/podium.svg')}}" alt="">
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda at aut blanditiis consectetur eos fuga in, maxime minima minus natus neque quis quo recusandae rem, rerum saepe sed tempora?
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-4 col-md-6 col-12 text-center pb-4">
+
+                    <div class="card">
+                        <img class="p-5" src="{{asset('icons/medal.svg')}}" alt="">
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda at aut blanditiis consectetur eos fuga in, maxime minima minus natus neque quis quo recusandae rem, rerum saepe sed tempora?
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
 
