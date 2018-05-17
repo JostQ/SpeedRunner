1fc24305c28829d4248a7e16b2251a78fb895db9
kingCharlemagne <37544755+kingCharlemagne@users.noreply.github.com>
2018-05-17 09:44:44 +0200

    bilal fait du caca

--- resources/views/actu/index.blade.php
+++ resources/views/actu/index.blade.php
@@ -1,8 +1,8 @@
-{{--@extends('layouts.app')--}}
+@extends('layouts.app')
 
-{{--@section('content')--}}
+@section('content')
     {{--file d'actu--}}
-<<<<<<< HEAD
+
     <section class="container pt-5 mt-5">
         <div class="row justify-content-center">
             <nav class="actu col-12">
@@ -32,41 +32,6 @@
                                     <button type="submit" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
-=======
-    <section id="actu" class="container pt-5 mt-5">
-        <div class="row">
-            <div class="col-12">
-                <div class="container ajActu ">
-                    <ul class="nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" id="pills-ajStatue-tab" data-toggle="pill" href="#pills-ajStatue"
-                               role="tab" aria-controls="pills-home" aria-selected="true">Créer une publication</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile"
-                               role="tab" aria-controls="pills-profile" aria-selected="false">Publier une Photo</a>
-                        </li>
-                    </ul>
-
-                    <div class="tab-content" id="pills-tabContent">
-
-                        <div class="tab-pane fade show active" id="pills-ajStatue"
-                             role="tabpanel" aria-labelledby="pills-ajStatue-tab">
-                            <form action="{{ url('/status-textarea') }}" method="post"
-                                  class="form-inline justify-content-center">
-                                @csrf
-                                <div class="col-10">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">Statut :</span>
-                                        </div>
-
-
-                                        <textarea class="form-control" aria-label="With textarea" name="statut"></textarea>
-                                        <button type="submit" class="btn " value="ok" name="postStatue">Poster</button>
-                                    </div>
->>>>>>> a1a5d8f3fa7fe51f6b29a3e992fbb15a2d19e99f
                                 </div>
                                 <form action="{{url('/post-status')}}" method="post" name="statut">
                                     @csrf
@@ -83,8 +48,6 @@
                             </div>
                         </div>
                     </div>
-
-<<<<<<< HEAD
                 </div>
             </nav>
 
@@ -99,34 +62,6 @@
                                 <div class="row">
                                     <div class="col-2">
                                         <img src="{{--img dans le bdd--}}" alt="profilPict">
-=======
-                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
-                               role="tab" aria-controls="nav-home" aria-selected="true">#Général</a>
-                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
-                               role="tab" aria-controls="nav-profile" aria-selected="false">#Amis</a>
-                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact"
-                               role="tab" aria-controls="nav-contact" aria-selected="false">#Ligue</a>
-
-                        </div>
-                    </nav>
-
-                    {{--file générale--}}
-                    <div class="tab-content" id="nav-tabContent">
-                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel"
-                             aria-labelledby="nav-home-tab">
-
-                            {{--@foreach()--}}
-                            <div class="card ">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-2">
-                                            <img src="{{--img dans le bdd--}}" alt="profilPict">
-                                        </div>
-                                        <div class="col-10">
-                                            <h5 class="card-title speedrun-title">Lorem
-                                                Ipsum{{--pseudo des utilisateurs--}}</h5>
-                                        </div>
->>>>>>> a1a5d8f3fa7fe51f6b29a3e992fbb15a2d19e99f
                                     </div>
                                     <div class="col-10">
                                         <h5 class="card-title speedrun-title">Lorem
@@ -198,8 +133,7 @@
             </div>
         </div>
     </section>
-
-{{--@endsection--}}
+@endsection
 
 @section('page-specific-scripts')
     <script defer>
