96415f45d1150cb7327bb53d8e105cf3f390c427
Maxime Simoncelli <36240090+maximesimoncelli@users.noreply.github.com>
2018-05-18 12:05:57 +0200

    Liste de courses + intéraction BDD

--- app/Http/Controllers/RoutesController.php
+++ app/Http/Controllers/RoutesController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class RoutesController extends Controller
 {
     public function index(){
 
+        $racesDone = DB::table('races')->where('users_id', Auth::user()->id)->get();
 
-
-        return view('routes.index');
+        return view('routes.index')
+            ->with('racesDone', $racesDone);
     }
 }
